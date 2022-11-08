89d23c239cddb46db54407230cb56d428cb831ee
matolgithub <89175955650@mail.ru>
2022-11-08 16:35:12 +0300

    Next commit to git.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from requests import get
+from grequests import get
+from grequests import request
+# from requests import get
 from fake_headers import Headers
 from random import choice
 from pprint import pprint
-# import grequests
 
 website = "https://www.avito.ru/moskva/kollektsionirovanie/voennye_veschi-ASgBAgICAUQchgE?cd=1&q=плитник"
 
@@ -15,17 +16,25 @@ def get_headers():
     return choice(headers_list)
 
 
+# # with requests
+# def main():
+#     header = get_headers()
+#     print(f"Random header: {header}")
+#     response = get(url=website, headers=header)
+#     pprint(response.content)
+#     print(response.status_code)
+
+# with grequests
 def main():
     header = get_headers()
-    print(f"Random header: {header}")
-    response = get(url=website, headers=header)
-    pprint(response.content)
-    print(response.status_code)
+    response = request(method="GET", url=website, headers=header)
+    pprint(response)
 
 
 if __name__ == "__main__":
     main()
 
+# run-command: time python3 main.py
 # # with requests:
 #     real
 #     0
@@ -40,3 +49,7 @@ if __name__ == "__main__":
 #     m0, 179
 #     s
 
+# with grequests:
+# real    0m0,370s
+# user    0m0,334s
+# sys     0m0,036s
